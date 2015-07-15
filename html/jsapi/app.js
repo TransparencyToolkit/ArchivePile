3cb285980586416bea96468544ce24fecacd521e
Brennan Novak <hi@brennannovak.com>
2015-07-15 18:24:49 +0200

    added more working HTML

---
+++ html/jsapi/app.js
@@ -0,0 +1,30 @@
+/* JS - Crypto */
+{% include("jsapi/crypto/init.js") %}
+{% include("jsapi/crypto/events.js") %}
+{% include("jsapi/crypto/find.js") %}
+{% include("jsapi/crypto/import.js") %}
+{% include("jsapi/crypto/modals.js") %}
+{% include("jsapi/crypto/tooltips.js") %}
+{% include("jsapi/crypto/ui.js") %}
+
+/* JS - Search */
+{% include("jsapi/search/init.js") %}
+{% include("jsapi/search/bulk_actions.js") %}
+{% include("jsapi/search/events.js") %}
+{% include("jsapi/search/display_modes.js") %}
+{% include("jsapi/search/selection_actions.js") %}
+{% include("jsapi/search/tooltips.js") %}
+{% include("jsapi/search/ui.js") %}
+
+/* JS - Tags */
+{% include("jsapi/tags/init.js") %}
+{% include("jsapi/tags/modals.js") %}
+{% include("jsapi/tags/events.js") %}
+{% include("jsapi/tags/tooltips.js") %}
+
+/* JS - Message */
+{% include("jsapi/message/init.js") %}
+{% include("jsapi/message/events.js") %}
+{% include("jsapi/message/message.js") %}
+{% include("jsapi/message/tooltips.js") %}
+{% include("jsapi/message/ui.js") %}
