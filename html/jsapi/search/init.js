fe1c90e0eb7b3bafd3bfd7f981dd0eef2a3fe0a9
Brennan Novak <hi@brennannovak.com>
2015-08-07 22:09:07 +0200

    various other lil tweaks and fixes

--- html/jsapi/search/init.js
+++ html/jsapi/search/init.js
@@ -30,35 +30,4 @@ Mailpile.Search.init = function() {
   // Tooltips
   Mailpile.Search.Tooltips.MessageTags();
 
-  /* STUFF Smari added for notifications
-  $("#pile-newmessages-notification").click(Mailpile.update_search);
-  */
-
-  EventLog.subscribe(".mail_source", function(ev) {
-    // bre: re-enabling this just for fun and to test the event subscription
-    //      code. This is broken in that it fails for non-English languages.
-    if (ev.message.indexOf("Rescanning:") != -1) {
-      $("#logo-bluemail").fadeOut(2000);
-      $("#logo-redmail").hide(2000);
-      $("#logo-greenmail").hide(3000);
-      $("#logo-bluemail").fadeIn(2000);
-      $("#logo-greenmail").fadeIn(4000);
-      $("#logo-redmail").fadeIn(6000);
-    }
-    $('.status-in-title').attr('title', ev.data.name + ': ' + ev.message);
-
-/*
-    if (ev.flags.indexOf("c") != -1 && ev.data.messages > 0) {
-      $("#pile-newmessages-notification").slideDown("slow");
-
-      if (Notification.permission == "granted") {
-        new Notification(ev.data.messages + "{{_(' new messages received')}}", { 
-            body:'{{_("Your pile is growing...")}}',
-            icon:'{{ config.sys.http_path }}/static/img/logo-color.png', 
-          }  
-        )
-      }
-    }
-*/
-  });
 };
