c893d21b8d7e51b5fb32bdab58851aff7549d1ce
Brennan Novak <hi@brennannovak.com>
2015-08-12 21:30:37 +0200

    made sidebar expand glitch go away

--- html/jsapi/ui/sidebar.js
+++ html/jsapi/ui/sidebar.js
@@ -1,22 +1,9 @@
 Mailpile.UI.Sidebar.SubtagsToggle = function(tid) {
-
-  // Show or Hide
-  if (_.indexOf(Mailpile.config.web.subtags_collapsed, tid) > -1) {
-    $('#sidebar-tag-' + tid).addClass('show-subtags');
-    $('#sidebar-tag-' + tid).find('a.sidebar-tag span.sidebar-tag-expand span').removeClass('icon-arrow-left').addClass('icon-arrow-down');
-    $('#sidebar-subtags-' + tid).slideDown('fast');
-    var collapsed = _.without(Mailpile.config.web.subtags_collapsed, tid);
+  if ($('#sidebar-subtags-' + tid).hasClass('hide')) {
+    $('#sidebar-tag-' + tid).find('a.sidebar-tag span.sidebar-tag-expand').html('-');
+    $('#sidebar-subtags-' + tid).removeClass('hide').slideDown('fast');
   } else {
-    $('#sidebar-tag-' + tid).removeClass('show-subtags');
-    $('#sidebar-tag-' + tid).find('a.sidebar-tag span.sidebar-tag-expand span').removeClass('icon-arrow-down').addClass('icon-arrow-left');
-    $('#sidebar-subtags-' + tid).slideUp('fast');
-    Mailpile.config.web.subtags_collapsed.push(tid);
-    var collapsed = Mailpile.config.web.subtags_collapsed;
+    $('#sidebar-tag-' + tid).find('a.sidebar-tag span.sidebar-tag-expand').html('+');
+    $('#sidebar-subtags-' + tid).addClass('hide').slideUp('fast');
   }
-
-  // Save to Config
-  Mailpile.config.web.subtags_collapsed = collapsed;
-  Mailpile.API.settings_set_post({ 'web.subtags_collapsed': collapsed }, function(result) { 
-
-  });
 };
