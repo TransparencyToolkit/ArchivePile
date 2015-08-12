bafdc499558f6a0d706b379d5a4cef636a106640
Brennan Novak <hi@brennannovak.com>
2015-08-12 02:11:37 +0200

    fixed icons from theme, removed some JS, added else for search_terms

--- html/jsapi/ui/init.js
+++ html/jsapi/ui/init.js
@@ -16,8 +16,6 @@ Mailpile.UI.init = function() {
 
   // Favicon
   var inbox = _.findWhere(Mailpile.instance.tags, {slug: 'inbox'});
-  var favicon = new Favico({animation:'popFade'});
-  favicon.badge(inbox.stats.new);
 
   // Show Typeahead
   Mailpile.activities.render_typeahead();
@@ -31,19 +29,11 @@ Mailpile.UI.init = function() {
     EventLog.timer.set({ time : 22500, autostart : false });
     EventLog.poll();
 
-    // Run Composer Autosave
-    if (Mailpile.instance.state.context_url === '/message/' || 
-        Mailpile.instance.state.context_url === '/message/draft/') {
-      Mailpile.Composer.AutosaveTimer.play();
-      Mailpile.Composer.AutosaveTimer.set({ time : 20000, autostart : true });
-    }
-
   }, 1000);
 
   /* Tooltips */
   Mailpile.UI.Tooltips.TopbarNav();
   Mailpile.UI.Tooltips.BulkActions();
-  Mailpile.UI.Tooltips.ComposeEmail();
 
 };
 
